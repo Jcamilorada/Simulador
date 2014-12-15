b555b0ee727194aa6059b5efc7536a58ce394c12
Juan Camilo Rada <jcamilorada@live.com>
2014-12-14 21:59:44 -0500

    Added test data information

--- src/main/java/simulator/restservices/InfusionController.java
+++ src/main/java/simulator/restservices/InfusionController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import simulator.services.infusion.InfusionService;
 import simulator.dto.CalculationRequestDTO;
@@ -21,7 +22,7 @@ public class InfusionController
         this.infusionService = infusionService;
     }
 
-    @RequestMapping("/solve")
+    @RequestMapping(value = "/solve", method = RequestMethod.PUT)
     public
     @ResponseBody
     CalculationResponseDTO getMesh(@RequestBody CalculationRequestDTO requestDTO)
