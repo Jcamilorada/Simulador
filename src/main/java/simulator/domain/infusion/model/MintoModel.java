506c45afd2e78b29a1c091d19a0033ecbc4b15b7
Alejandro Valdes <alejandrovaldes@live.com>
2015-01-05 22:56:39 -0500

    Fixed rounding error and some typos on MintoModel

--- src/main/java/simulator/domain/infusion/model/MintoModel.java
+++ src/main/java/simulator/domain/infusion/model/MintoModel.java
@@ -31,24 +31,26 @@ public class MintoModel implements IModel
         double lbm;
         if (Gender.Male.equals(patient.getGender()))
         {
-            lbm = (patient.getWeight() * 1.1D) - 128D * Math.pow(patient.getWeight() / patient.getHeight(), 2);
+            lbm = (patient.getWeight() * 1.1D)
+                - 128D * Math.pow((double) patient.getWeight() / (double) patient.getHeight(), 2);
         }
         else
         {
-            lbm = (patient.getWeight() * 1.07D) - 148D * Math.pow(patient.getWeight() / patient.getHeight(), 2);
+            lbm = (patient.getWeight() * 1.07D)
+                - 148D * Math.pow((double) patient.getWeight() / (double) patient.getHeight(), 2);
         }
         double cl1 = 2.6D - 0.0162D * (patient.getAge() - 40D) + 0.0191D * (lbm - 55D);
-        double cl2 = 2.05D / 0.0301D * (patient.getAge() / 40D);
-        double cl3 = 0.076D - 0.00113D * (patient.getAge() / 40D);
+        double cl2 = 2.05D - 0.0301D * (patient.getAge() - 40D);
+        double cl3 = 0.076D - 0.00113D * (patient.getAge() - 40D);
         double centralVolume = 5.1D - 0.0201D * (patient.getAge() - 40D) + 0.072D * (lbm - 55D);
         double V2 = 9.82D - 0.0811D * (patient.getAge() - 40D) + 0.108D * (lbm -  55D);
         double V3 = 5.42D;
         double V4 = centralVolume / 10000D;
 
-        weightedGraph.put(1, 3, (cl3 / centralVolume) / 60D);
         weightedGraph.put(1, 0, (cl1 / centralVolume) / 60D);
         weightedGraph.put(1, 2, (cl2 / centralVolume) / 60D);
-        weightedGraph.put(1, 4, (weightedGraph.get(1, 0) / 10000D) / 60D);
+        weightedGraph.put(1, 3, (cl3 / centralVolume) / 60D);
+        weightedGraph.put(1, 4, ((cl1 / centralVolume) / 10000D) / 60D);
         weightedGraph.put(2, 1, (cl2 / V2) / 60D);
         weightedGraph.put(3, 1, (cl3 / V3) / 60D);
         weightedGraph.put(4, 1, (0.595D - 0.007D * (patient.getAge() - 40)) / 60D);
--- src/test/java/simulator/domain/infusion/calculations/PumpSolverTest.java
+++ src/test/java/simulator/domain/infusion/calculations/PumpSolverTest.java
@@ -8,11 +8,13 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import simulator.configuration.PumpProperties;
+import simulator.domain.infusion.Gender;
 import simulator.domain.infusion.Patient;
 import simulator.domain.infusion.CalculationRequest;
 import simulator.domain.infusion.CalculationResponse;
 import simulator.domain.infusion.model.IModel;
 import simulator.domain.infusion.InfusionRequest;
+import simulator.domain.infusion.model.MintoModel;
 import simulator.domain.infusion.model.Model;
 import simulator.domain.infusion.model.SchiderModel;
 
@@ -27,11 +29,12 @@ public class PumpSolverTest
     private static final double MAXIMUM_INFUSION = 10D;
     private static final double MINIMUM_INFUSION = 1D;
     private static final int MAXIMUM_TIME = 10000;
-    private CalculationRequest request;
-
-    private PumpSolver testInstance;
 
     @Mock PumpProperties mockPumpProperties;
+
+    private Patient patient;
+
+    private PumpSolver testInstance;
 
     @Before
     public void setUp()
@@ -42,23 +45,42 @@ public class PumpSolverTest
         when(mockPumpProperties.getMaximumInfusion()).thenReturn(MAXIMUM_INFUSION);
         when(mockPumpProperties.getMinimumInfusion()).thenReturn(MINIMUM_INFUSION);
         when(mockPumpProperties.getMaximumTime()).thenReturn(MAXIMUM_TIME);
-        Patient patient = new Patient();
+        patient = new Patient();
         patient.setWeight(55);
         patient.setHeight(160);
         patient.setAge(22);
+        patient.setGender(Gender.Male);
         IModel model = new SchiderModel(patient);
 
         testInstance = new PumpSolver(mockPumpProperties);
         testInstance.setModel(model);
-        request = new CalculationRequest();
-        request.setModel(Model.Schider);
-        request.setDeltaTime(50);
-        request.setInfusionRequestList(Arrays.asList(new InfusionRequest(0, 500, 2), new InfusionRequest(700, 1000, 3)));
     }
 
     @Test
     public void testSolve()
     {
+        CalculationRequest request = new CalculationRequest();
+        request.setModel(Model.Schider);
+        request.setDeltaTime(50);
+        request.setPatient(patient);
+        request.setInfusionRequestList(Arrays.asList(new InfusionRequest(0, 500, 2), new InfusionRequest(700, 1000, 3)));
         CalculationResponse response = testInstance.solve(request);
     }
+
+    @Test
+    public void testSolveWithMinto()
+    {
+        CalculationRequest request = new CalculationRequest();
+        request.setModel(Model.Minto);
+        request.setDeltaTime(300);
+        request.setInfusionRequestList(Arrays.asList(
+            new InfusionRequest(0, 360, 3.86),
+            new InfusionRequest(360, 660, 3.51),
+            new InfusionRequest(1860, 2160, 2.69)));
+        request.setPatient(patient);
+        IModel model = new MintoModel(patient);
+        testInstance.setModel(model);
+        CalculationResponse response = testInstance.solve(request);
+    }
+
 }
